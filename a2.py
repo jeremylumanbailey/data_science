6cb6bb5ef6af4188ecb49aada6df09fa3026409c
Jeremy Latitude E6410 <jeremylumanbailey@gmail.com>
2019-09-18 16:54:02 -0400

    impute_mean function works properly

--- a2.py
+++ a2.py
@@ -2,6 +2,7 @@ print("START")
 
 # Load the Pandas libraries with alias 'pd'
 import pandas as pd
+
 # Read data from file 'filename.csv'
 # (in the same directory that your python process is based)
 # Control delimiters, rows, column names with read_csv (see later)
@@ -12,25 +13,37 @@ print(mean_imputation_table.head())
 print()
 num_of_rows = len(mean_imputation_table.index)
 
-sum_values_in_f3 = 0
-valid_values_in_f3 = 0
-i = 0
-while i < num_of_rows:
-    x = mean_imputation_table.loc[i,:].values[2]
-    if x != "?":
-        valid_values_in_f3 += 1
-        sum_values_in_f3 += float(x)
-    i += 1
 
-while i < num_of_rows:
-    x = mean_imputation_table.loc[i,:].values[2]
-    if x != "?":
-        valid_values_in_f3 += 1
-        sum_values_in_f3 += float(x)
-    i += 1
+def get_mean(column_to_find_mean):
+    sum_values_in_f3 = 0
+    valid_values_in_f3 = 0
+    i = 0
+    while i < num_of_rows:
+        x = mean_imputation_table.loc[i, :].values[column_to_find_mean]
+        if x != "?":
+            valid_values_in_f3 += 1
+            sum_values_in_f3 += float(x)
+        i += 1
 
-print(sum_values_in_f3 / valid_values_in_f3)
-print(valid_values_in_f3)
+    return(sum_values_in_f3 / valid_values_in_f3)  # print(valid_values_in_f3)
+
+
+def impute_mean(column_to_impute):
+    mean_to_impute = get_mean(column_to_impute)
+    i = 0
+    while i < num_of_rows:
+        x = mean_imputation_table.loc[i, :].values[column_to_impute]
+        if x == "?":
+            mean_imputation_table.loc[i, :].values[column_to_impute] = mean_to_impute
+        i += 1
+
+
+
+impute_mean(2)
+
+print(mean_imputation_table.head())
+print()
+
 print("Number of rows not including header:", num_of_rows)
 
 # Use DATATABLE.to_csv("V00880079....csv") to create tables
